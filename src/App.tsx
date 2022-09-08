1032b26a3938ab1d9026dbaa30249374a415192f
Kemal Demirtas <kemal.demirtas@trr.se>
2022-09-08 23:11:19 +0200

    starting of the work

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.sass";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { Container, CssBaseline, Typography } from "@mui/material";
+
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./Styles";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container>
+        <Typography variant="h1" align="center">
+          Hej
+        </Typography>
+      </Container>
+    </ThemeProvider>
   );
 }
 
